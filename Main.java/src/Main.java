ab9bd33130aabf622f413dfbafaddc4233d290cc
Seu Nome <seuemail@email.com>
2026-04-14 18:53:44 -0300

    feat: menu com listar funcionando

--- Main.java/src/Main.java
+++ Main.java/src/Main.java
@@ -1,6 +1,54 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciando projeto CRUD");
+        Scanner scanner = new Scanner(System.in);
+        int idRemover = 1;
+        int idUpdate = 2;
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario(1, "Daniel"));
+        usuarios.add(new Usuario(2, "Luiz"));
+        int opcao = 0;
+        while (opcao != 5) {
+            System.out.println("Digite uma opção: ");
+            System.out.println("1-Cadastrar");
+            System.out.println("2-Listar");
+            System.out.println("3-Deletar");
+            System.out.println("4-Atualizar");
+            System.out.println("5-Sair");
+
+            opcao = scanner.nextInt();
+            switch (opcao) {
+                case 1:
+                    // cadastrar
+                    break;
+
+                case 2:
+                    for (Usuario u : usuarios) {
+                        System.out.println(u);
+                    }
+                    break;
+
+                case 3:
+                    // deletar
+                    break;
+
+                case 4:
+                    // atualizar
+                    break;
+
+                case 5:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida");
+            }
+
+        }
+
     }
 }
+
---
+++ Main.java/src/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+    private int id;
+    private String nome;
+
+    @Override
+    public String toString() {
+        return id + " - " + nome;
+    }
+    public Usuario(int id,String nome){
+        this.id=id;
+        this.nome=nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
+
