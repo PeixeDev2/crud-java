b1b325c76c6be14a34c226c846aef8971d32a581
Seu Nome <seuemail@email.com>
2026-04-15 19:44:59 -0300

    feat: implementa delete e update no menu

--- Main.java/src/Main.java
+++ Main.java/src/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciando projeto CRUD");
         Scanner scanner = new Scanner(System.in);
-        int idRemover = 1;
-        int idUpdate = 2;
+        int iddelete=0;
+        int idmodification=0;
         ArrayList<Usuario> usuarios = new ArrayList<>();
         usuarios.add(new Usuario(1, "Daniel"));
         usuarios.add(new Usuario(2, "Luiz"));
@@ -32,11 +32,44 @@ public class Main {
                     break;
 
                 case 3:
-                    // deletar
-                    break;
+                    System.out.println("Digite o id do usuário que deseja deletar:");
+                    iddelete= scanner.nextInt();
+                    boolean encontrado = false;
+
+                    for (Usuario u: usuarios){
+                        if (iddelete == u.getId()){
+                            usuarios.remove(u);
+                            encontrado = true;
+                            System.out.println("Usuário removido com sucesso!");
+                            break;
+                        }
+                    }
+
+                    if (!encontrado){
+                        System.out.println("Usuário não encontrado!");
+                    }break;
 
                 case 4:
-                    // atualizar
+                    System.out.println("Digite o id do usuário que deseja fazer as modificções:");
+                    idmodification= scanner.nextInt();
+                    scanner.nextLine();
+                    boolean encontradomodi=false;
+                    for (Usuario u: usuarios){
+                        if (idmodification == u.getId()){
+                            System.out.println("Digite aqui o novo nome desse usuário:");
+                            String new_name = scanner.nextLine();
+                            u.setNome(new_name);
+                            System.out.println("O novo nome desse usuário é:"+ new_name);
+                            encontradomodi = true;
+                            System.out.println("Usuário modificado com sucesso!");
+                            break;
+                        }
+
+                    }
+                    if (!encontradomodi){
+                        System.out.println("Usuário não encontrado!");
+                    }
+
                     break;
 
                 case 5:
